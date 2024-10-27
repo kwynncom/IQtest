9ee55a9a7c822c27b6b5446dba1013d16a2465ca
Kwynn Buess <buessresume@gmail.com>
2024-10-27 06:33:40 -0400

    source code link and older code backlink

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A web implementation of someone else's IQ test
 
 https://kwynn.com/t/23/04/IQ/ - running at
+
+The older version:
+
+https://github.com/kwynncom/code-fragments/tree/034f73d570dff6249842cf18cc2f95de846bbc78/bucket_202405/IQ
--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 			<li><a href='./front/menuMain.php'>take (my implementation of) the test</a></li>
 			<li><a href='/t/23/04/IQ/extra/instructions.html'>directions</a></li>
 			<li><a href='./extra/worksCited.html'>works cited</a></li>
+			<li><a href='https://github.com/kwynncom/IQtest'>source code</a></li>
 		</ul>
 	</section>
 </body>
